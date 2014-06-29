4727b69e86428b1df4d01b5a98373e6f196df6eb
Steve Jamesson <steve@groovywebdesign.com>
2014-06-29 12:33:51 -0400

    example of partial on content pages; meta author/description fallback to global data unless local data is defined

---
+++ README.md
@@ -0,0 +1,3 @@
+# Static Starter Project
+
+## Quickly build static sites with [Harp](http://harpjs.com), [Bootstrap](http://getbootstrap.com), [Bower](http://bower.io/), and [Gulp](http://gulpjs.com)
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var rsync = require('rsyncwrapper').rsync;
+
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+var changed = require('gulp-changed');
